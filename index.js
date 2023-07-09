4a87a3e30287d02a0100f8c216d4441a5c3370a9
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-07-09 16:13:48 +0600

    news and categoryNews api created

--- index.js
+++ index.js
@@ -6,9 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('news-portal api ruunning')
+})
+
+// news api
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+    console.log(req.params.id);
+})
+
+// category-news api with id
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '0') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
 })
 
 // news categories
